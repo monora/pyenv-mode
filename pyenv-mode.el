afc0c92b88a216146ca464a7cbc4fbc34f327648
Horst Duchene <monora@gmail.com>
2022-02-11 16:04:52 +0100

    fix: do not collide keybindings with org-mode

--- pyenv-mode.el
+++ pyenv-mode.el
@@ -79,8 +79,8 @@
 
 (defvar pyenv-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c C-s") 'pyenv-mode-set)
-    (define-key map (kbd "C-c C-u") 'pyenv-mode-unset)
+    (define-key map (kbd "C-c a C-s") 'pyenv-mode-set)
+    (define-key map (kbd "C-c a C-u") 'pyenv-mode-unset)
     map)
   "Keymap for pyenv-mode.")
 
